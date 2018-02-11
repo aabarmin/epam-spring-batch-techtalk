0dc9e99452f3b0cff231655d656832eace250f77
Aleksandr Barmin <barmin.alexander@gmail.com>
2018-02-11 19:16:05 +0300

    Example 11 works

---
+++ src/main/java/com/epam/techtalk/spring/batch/example11/Example11Application.java
@@ -0,0 +1,16 @@
+package com.epam.techtalk.spring.batch.example11;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Example11Application {
+    public static void main(String[] args) throws Exception {
+        final ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("example11/example11.xml");
+
+        final JobLauncher jobLauncher = context.getBean(JobLauncher.class);
+        final Job sequentialJob = context.getBean("example11Job", Job.class);
+        jobLauncher.run(sequentialJob, new JobParameters());
+    }
+}
